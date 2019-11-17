b9b63998e80eef0cdda08f7fe9d86076bcf369d3
SakuMyl <myllymakisaku@gmail.com>
2019-11-17 04:24:50 +0200

    fix card click bug

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,37 +4,37 @@ import cards from '../cards';
 
 const GameBoard = (props) => {
 
-  const [chosenCard, setChosenCard] = useState(null)
-  const [hand, setHand] = useState(props.G.player.hand)
+    const [chosenCard, setChosenCard] = useState(null)
+    const [hand, setHand] = useState(props.G.player.hand)
 
-  useEffect(() => {
-    setHand(props.G.player.hand)
-  }, [props.G.player.hand])
+    const consumeCard = () => {
+        if(chosenCard) {
+            const card = props.G.player.hand.find(z => z.id === chosenCard)
+            if(card) {
+                const effect = card[props.G.mode].effect
+                props.moves.clickCard(effect)
+                props.G.player.hand = props.G.player.hand.filter(z => z.id !== chosenCard)
+                setHand(props.G.player.hand)
+                props.events.endTurn()
+                setChosenCard(null) 
+            }
+        }
+    }
 
-const consumeCard = () => {
-  const effect = props.G.player.hand.find(z => z.id === chosenCard)[props.G.mode].effect
-  props.G.player.hand = props.G.player.hand.filter(z => z.id !== chosenCard)
-  setHand(props.G.player.hand)
-  props.moves.clickCard(effect)
-  props.events.endTurn()
-}
+    return (
+        <div>
+            <div className='view'>
+                <div className='board' onClick={consumeCard}>
+                    <img src='\images\hearthstoneboard.jpg' style={{width:"100vw"}}></img>
+                </div>
+                <div className='bottomBar'>
+                    <img className='healthJug' src="/images/health4.png"></img>
 
-  return (
-    <div>
-      <div className='view'>
-        <div className='board' onClick={consumeCard}>
-          <img src='\images\hearthstoneboard.jpg' style={{width:"100vw"}}></img>
+                    <Hand mode={props.G.mode} onClick={(id) => setChosenCard(id)} className='hand' hand={hand}/>
+                </div>
+            </div>
         </div>
-          <div className='bottomBar'>
-            <img className='healthJug' src="/images/health4.png"></img>
-
-            <Hand mode={props.G.mode} onClick={(id) => setChosenCard(id)} className='hand' hand={hand}/>
-          </div>
-
-        </div>
-         
-    </div>
-  )
+    )
 
 }
 
